5cf27b63f757a214211b0d782217ae29152e06e3
hmelnov777 <82603496+hmelnov777@users.noreply.github.com>
2021-07-29 15:52:20 +0300

    Мобильная верстка

--- js/main.js
+++ js/main.js
@@ -5,10 +5,20 @@
         watchSlidesVisibility: true,
         watchSlidesProgress: true,
       });
-      var swiper2 = new Swiper(".mySwiper2", {
+var swiper2 = new Swiper(".mySwiper2", {
         spaceBetween: 10,
        
         thumbs: {
           swiper: swiper,
         },
-      });
+});
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-pannel");
+const menuCloseBtn = document.querySelector(".close-menu");
+menuBtn.addEventListener("click", () => {
+menu.classList.toggle("is-open");
+});
+
+menuCloseBtn.addEventListener("click", ()=>{
+  menu.classList.toggle("is-open");
+})
